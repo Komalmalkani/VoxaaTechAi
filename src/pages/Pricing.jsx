987e902a38990b57202c9e29262f5d5b56546303
Komal Malkani <131575983+Komalmalkani@users.noreply.github.com>
2025-03-04 22:52:44 -0800

    changes

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -35,10 +35,10 @@ const Pricing = () => {
       </motion.h1>
 
       <motion.p
-        className="lead text-light"
-        initial={{ opacity: 0, y: 20 }}
+        className="lead text-light py-3"
+        initial={{ opacity: 0.5, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 1, delay: 0.4 }}
+        transition={{ duration: 1, delay: 0.5 }}
         viewport={{ amount: 0.2 }}
       >
         Empower your business with our flexible and affordable AI voice agent plans.
